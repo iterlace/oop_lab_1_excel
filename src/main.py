6d5b001ce1f393a22320410b6256fcf64927d2ec
Evgeniy Goncharuck <iterlace@gmail.com>
2021-11-21 16:17:02 +0200

    ukrainian localization, added "About" menu

--- src/main.py
+++ src/main.py
@@ -18,42 +18,56 @@ class MainWindow(QMainWindow):
 
     def init_actions(self):
         self.open_action = QAction(self.table_widget)
-        self.open_action.setText("&Open")
+        self.open_action.setText("&Відкрити")
         self.open_action.triggered.connect(self.table_widget.on_open)
 
         self.save_action = QAction(self.table_widget)
-        self.save_action.setText("&Save")
+        self.save_action.setText("&Зберегти")
         self.save_action.triggered.connect(self.table_widget.on_save)
 
-        self.usage_action = QAction(self.table_widget)
-        self.usage_action.setText("&Usage")
-        self.usage_action.triggered.connect(self.on_usage)
+        self.help_action = QAction(self.table_widget)
+        self.help_action.setText("&Допомога")
+        self.help_action.triggered.connect(self.on_help)
+
+        self.about_action = QAction(self.table_widget)
+        self.about_action.setText("&Про додаток")
+        self.about_action.triggered.connect(self.on_about)
 
     def init_menubar(self):
         menubar = self.menuBar()
-        file_menu = QMenu("&File", self)
+        file_menu = QMenu("&Файл", self)
         menubar.addMenu(file_menu)
 
         file_menu.addAction(self.open_action)
         file_menu.addAction(self.save_action)
 
-        file_menu = QMenu("&Help", self)
+        file_menu = QMenu("&Допомога", self)
         menubar.addMenu(file_menu)
-        file_menu.addAction(self.usage_action)
+        file_menu.addAction(self.about_action)
+        file_menu.addAction(self.help_action)
 
-    def on_usage(self):
+    def on_about(self):
         QMessageBox.information(
             self,
-            "Usage",
-            f"Available data types, operators and functions:\n"
-            f" - Available data types: integer and float\n"
-            f" - Infix binary operators: 1+1, 1-1, 1*1, 1/1\n"
-            f" - Prefix unary operators: ++1, --1\n"
-            f" - NOT expression: !1\n"
-            f" - Comparisons: 1<1, 1>1, 1=1\n"
-            f" - Parenthesis: (1+1)\n"
-            f" - Cell references: F11, E9\n"
-            f" - Functions: max(1, 2), min(5, 1)",
+            "Про програму",
+            f"Версія: невизначена\n"
+            f"Розроблено: Джоном Доу\n"
+            f"Вихідний код: https://github.com/iterlace/oop_lab_1_excel"
+        )
+
+    def on_help(self):
+        QMessageBox.information(
+            self,
+            "Допомога",
+            f"Доступні типи даних, оператори та функції:\n"
+            f" - Типи даних: integer and float\n"
+            f" - Інфіксні бінарні оператори: 1+1, 1-1, 1*1, 1/1\n"
+            f" - Префіксні унарні оператори: ++1, --1\n"
+            f" - Вираз НЕ: !1\n"
+            f" - Порівняння: 1<1, 1>1, 1=1\n"
+            f" - Дужки: (1+1)\n"
+            f" - Посилання на клітинки: F11, E9\n"
+            f" - Функції: max(1, 2), min(5, 1)",
         )
 
 
